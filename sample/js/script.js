68f50fffafacd9aafb194a971a0c94003056c5d5
kopo4eweb <kopo4e.web@yandex.ru>
2016-02-10 17:37:10 +0300

    Addition example using plugin.

---
+++ sample/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#example_1').simpleParallax({ parallaxRun: 'up' });
+    $('#example_2').simpleParallax({ parallaxRun: 'down' });
+    $('#example_3').simpleParallax({ parallaxRun: 'up', parallaxZoom: '+' });
+    $('#example_4').simpleParallax({ parallaxArea: 80 });
+});
